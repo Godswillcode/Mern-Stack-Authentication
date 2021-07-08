cf8ab44095f5abe6f268cee33349b08ae5900e40
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-08 14:33:49 +0100

    working on db

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,22 +23,32 @@ exports.register = async (req, res, next) => {
 };
 
 exports.login = async (req, res, next) => {
-   const {email, password} = req.body
+  const { email, password } = req.body;
 
-   if(!email || !password) {
-     res.status(400).json({ success: false, error: "Please provide email and password" })
-   }
+  if (!email || !password) {
+    res
+      .status(400)
+      .json({ success: false, error: "Please provide email and password" });
+  }
 
-   try {
-     const user = await User.findOne({email}).select("+password")
-     if(!user) {
-       res.status(400).json({success: false, error: "Invalid credentials"})
-     }
-     const isMatch =
-   } catch (error) {
-     
-   }
+  try {
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      res.status(400).json({ success: false, error: "Invalid credentials" });
+    }
+    const isMatch = await user.matchPassword(password);
 
+    if (!isMatch) {
+      res.status(404).json({ success: false, error: "Invalid credentials" });
+    }
+
+    res.status(200).json({
+      success: true,
+      token: "fhfjfj373djdj",
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
 };
 
 exports.forgotpassword = (req, res, next) => {
