ab3c47095ca1f690858f8a19ca0fdcf41d5bc723
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-08 13:31:46 +0100

    working on model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -26,15 +26,15 @@ const UserSchema = new mongoose.Schema({
   resetPasswordExpire: Date,
 });
 
-UserSchema.pre("save", async function(next) {
-    if(!this.isModified("password")) {
-        next()
-    }
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
 
   const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt)
-   
-})
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 const User = mongoose.model("User", UserSchema);
 
