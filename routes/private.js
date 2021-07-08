63cdc8314ddca997989f66906b929c5b2b03b87b
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-08 16:24:47 +0100

    working on protected route

--- routes/private.js
+++ routes/private.js
@@ -1,6 +1,7 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const { getPrivateData } = require("../controllers/private");
 
 router.route("/").get();
 
-module.exports = router
+module.exports = router;
