c7049d855e5215bc1a355cc8cd6c1133bca80841
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-08 16:05:39 +0100

    working on token

---
+++ middleware/auth.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken")
+const User = require("../models/User")
+const ErrorResponse = require("../utils/errorResponse")
+
+exports.protect = async (req, res, next) => [
+    let token;
+
+    if() {
+        token = 
+    }
+]
