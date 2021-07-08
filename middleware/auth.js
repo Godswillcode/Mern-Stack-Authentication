1671e64a755fe491087fe3dd91491bb955c3ff2e
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-08 16:20:08 +0100

    working on protected route

--- middleware/auth.js
+++ middleware/auth.js
@@ -17,14 +17,16 @@ exports.protect = async (req, res, next) => {
   }
 
   try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      const user = await User.findById(decoded.id)
+    const user = await User.findById(decoded.id);
 
-      if(!user) {
-        return next(new ErrorResponse("No user found with this id", 404));
-      }
+    if (!user) {
+      return next(new ErrorResponse("No user found with this id", 404));
+    }
+
+    req.user = user;
   } catch (error) {
-      
+    return next(new ErrorResponse("Not authorized to access this route", 401));
   }
 };
