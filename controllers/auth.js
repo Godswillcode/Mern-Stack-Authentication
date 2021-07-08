d5fec6b1d088bddede5a388e79dd83191472668d
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-08 12:05:09 +0100

    working on controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,6 +10,6 @@ exports.forgotpassword = (req, res, next) => {
     res.send("Forgot password Route")
 };
 
-exports.register = (req, res, next) => {
-    res.send("Register Route")
+exports.resetpassword = (req, res, next) => {
+    res.send("Reset password Route")
 };
