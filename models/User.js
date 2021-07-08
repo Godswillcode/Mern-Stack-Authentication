fc27fec984993a69537f23856d5af988184a46a4
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-08 12:51:17 +0100

    working on model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-    
+    username: {
+        type: String,
+        require: [true, "Please provide a username"]
+    },
+    email: {
+        type: String,
+        require: [true, "Please provide an email"]
+        unique: true,
+        match: [
+             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+        ]
+    }
 })
