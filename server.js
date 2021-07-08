310cfb2892ac87d8110f36611150e21ed69b07a1
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-08 14:06:27 +0100

    working on db

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-require('dotenv').config({path: "./config.env"})
-const express = require('express')
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const connectDB = require("./config/db");
 
-const app = express()
+// connect DB
+connectDB();
 
-app.use(express.json())
+const app = express();
+
+app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Logged Error: ${err}`);
+  server.close(() => process.exit(1));
+});
