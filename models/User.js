dab41ef2e19c4288c284d43fc18b69344d70387f
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-08 13:11:54 +0100

    working on model

--- models/User.js
+++ models/User.js
@@ -1,17 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        require: [true, "Please provide a username"]
-    },
-    email: {
-        type: String,
-        require: [true, "Please provide an email"]
-        unique: true,
-        match: [
-             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  username: {
+    type: String,
+    require: [true, "Please provide a username"],
+  },
+  email: {
+    type: String,
+    require: [true, "Please provide an email"],
+    unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please provide a valid email",
-        ]
-    }
-})
+    ],
+  },
+  password: {
+      type: String,
+      require: [true, "Please provide a password"],
+      minlength: 6,
+      select: false
+  },
+  
+});
