ae0a62bdc1d33b456b3c926d1117694056d71a47
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-08 12:12:36 +0100

    working on controller

--- routes/auth.js
+++ routes/auth.js
@@ -2,3 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const {register, login, forgotpassword, resetpassword} = require('../controllers/auth')
+
+router.route("/register").post(register)
+
+router.route("/login").post(login)
+
+router.route("/register").post(register)
